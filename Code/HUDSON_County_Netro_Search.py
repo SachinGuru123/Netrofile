ee5e699acb2323cb64421c86b250ef824ede2846
SachinGuru222 <Sachinguru222@gmail.com>
2023-04-11 11:08:46 +0530

    Changes 6

--- Code/HUDSON_County_Netro_Search.py
+++ Code/HUDSON_County_Netro_Search.py
@@ -4,20 +4,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import webbrowser
+from selenium.webdriver.support.ui import WebDriverWait
 
-#dataframe1 = pd.read_excel('D:\\Title_Files\\Input\\Hudson_county.xlsx')
-#data=dataframe1['Doc_No'][0]
-#print(data)
+dataframe1 = pd.read_excel('D:\\Title_Files\\Input\\Hudson_county.xlsx')
+data=dataframe1['Doc_No'][0]
+print(data)
 
 
 chrome_driver = 'D:\Title_Files\chromedriver_win32\chromedriver.exe'
 driver = webdriver.Chrome(chrome_driver)
 
 driver.maximize_window()
-driver.get('https://acclaim.hcnj.us/AcclaimWeb/')
+driver.get('https://officialrecords.broward.org/AcclaimWeb/')
 
-if driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[1]/div[1]/div/form/div/input'):
-    driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/div[1]/div/form/div/input').click()
-    print("Clicked")
-print("Not Clicked")
+driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div[2]/div/div[4]/h4/button/img').click()
+
+
+driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/form/div/input').click()
+
+
+
+
 input()
